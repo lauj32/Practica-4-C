203ba0f5bc94b0f4fb554a1bbc1c0cf62b04c462
lauj32 <105667937+lauj32@users.noreply.github.com>
2022-06-09 12:52:58 -0300

    Create punto6.h

---
+++ punto6.h
@@ -0,0 +1,11 @@
+typedef enum month {Enero = 1,Febrero,Marzo,Abril,Mayo,Junio,Julio,Agosto,Septiembre,Octubre,Noviembre,Diciembre,month_top} t_month;
+
+
+
+struct t_date {
+	
+		short day;
+		t_month month;
+		long year;
+	
+	} ;
